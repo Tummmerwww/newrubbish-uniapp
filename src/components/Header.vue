c474eb503c695c6eb7562e17dcaf0598952c8971
Tummmerwww <148954856+Tummmerwww@users.noreply.github.com>
2026-03-24 14:17:36 +0800

    Create reusable header component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>Reusable Header Component</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+header {
+  background-color: lightblue;
+  padding: 10px;
+  text-align: center;
+}
+</style>
